0dc500d587927276bd39819682f3f21ff03564c4
Caspar Asger Mortensen <caspartaw@gmail.com>
2021-04-26 15:13:45 +0200

    Navigation added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainNavComponent } from './main-nav/main-nav.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material.module';
+import { AppComponent } from './app.component';
+import { SidenavListComponent} from './navigation/sidenav-list/sidenav-list.component';
+import { HeaderComponent } from './navigation/header/header.component'
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { AppRoutingModule } from './app-routing.module';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    SidenavListComponent,
+    HeaderComponent,
+    WelcomeComponent,
+    SignupComponent,
+    LoginComponent
+
+
   ],
   imports: [
     BrowserModule,
-    MatToolbarModule,
-    MatButtonModule,
+    MaterialModule,
     BrowserAnimationsModule,
     LayoutModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
