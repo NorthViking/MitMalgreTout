7423b933c8e82328b143226597280f05dc79fc14
Caspar Asger Mortensen <caspartaw@gmail.com>
2021-05-12 17:49:22 +0200

    post funktion

--- backend/routes/mediaPosts.js
+++ backend/routes/mediaPosts.js
@@ -1,53 +1,100 @@
-const express = require('express');
-const multer = require('multer');
+const express = require("express");
+const multer = require("multer");
 
-const  Post  = require('../models/image');
+const Post = require("../models/image");
 
 const router = express.Router();
 
 const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpg',
-  'image/jpg': 'jpg'
+  "image/png": "png",
+  "image/jpeg": "jpg",
+  "image/jpg": "jpg",
 };
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const isValid = MIME_TYPE_MAP[file.mimetype];
     let error = new Error("invalid mime type");
-    if(isValid) {
+    if (isValid) {
       error = null;
     }
-    cb(error, 'backend/media');
+    cb(error, "backend/media");
   },
   filename: (req, file, cb) => {
-    const name = file.originalname
-    .toLowerCase()
-    .split(' ')
-    .join('-');
+    const name = file.originalname.toLowerCase().split(" ").join("-");
     const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + '-' + Date.now() + '-' + ext);
-  }
+    cb(null, name + "-" + Date.now() + "-" + ext);
+  },
 });
 
 router.post(
   "",
-  multer({storage: storage}).single("image"),
+  multer({ storage: storage }).single("image"),
   (req, res, next) => {
+    const url = req.protocol + "://" + req.get("host");
     const post = new Post({
       title: req.body.title,
+      imagePath: url + "/media/" + req.file.filename,
       description: req.body.description
     });
-    post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'post added successfully',
-      post:{
-        ...createdPost,
-        id: createdPost._id
-      }
+    post.save().then((createdPost) => {
+      res.status(201).json({
+        message: "post added successfully",
+        post: {
+          ...createdPost,
+          id: createdPost._id
+        }
+      });
+    });
+  }
+);
+
+router.put(
+  "/:id",
+  multer({ storage: storage }).single("image"),
+  (req, res, next) => {
+    let imagePath = req.body.imagePath;
+    if (req.file) {
+      const url = req.protocol + "://" + req.get("host");
+      imagePath = url + "/media/" + req.file.filename;
+    }
+    const post = new Post({
+      _id: req.body.id,
+      title: req.body.title,
+      imagePath: imagePath,
+      description: req.body.description
+    });
+    console.log(post);
+    Post.updateOne({ _id: req.params.id }, post).then((result) => {
+      res.status(200).json({ message: "update successful" });
+    });
+  }
+);
+
+router.get("", (req, res, next) => {
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Posts Fetched successfully",
+      posts: documents,
     });
   });
-
 });
 
+router.get("/:id", (req, res, next) => {
+  Post.findById(req.params.id).then((post) => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "Post Not found" });
+    }
+  });
+});
 
+router.delete("/:id", (req, res, next) => {
+  Post.deleteOne({_id: req.params.id}).then(result =>{
+    console.log(result);
+    res.status(200).json({message:"Media deleted"});
+  });
+});
+
+module.exports = router;
--- src/app/galleri/personal-galleri/personal-galleri.component.ts
+++ src/app/galleri/personal-galleri/personal-galleri.component.ts
@@ -1,40 +1,70 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms'
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from "@angular/router"
 
-import {GalleriService} from '../galleri.service'
-import { mimeType } from './mime-type.validator'
+import { GalleriService } from '../galleri.service';
+import { Galleri } from '../galleri.model'
+import { mimeType } from './mime-type.validator';
+
 
 @Component({
   selector: 'app-personal-galleri',
   templateUrl: './personal-galleri.component.html',
-  styleUrls: ['./personal-galleri.component.css']
+  styleUrls: ['./personal-galleri.component.css'],
 })
 export class PersonalGalleriComponent implements OnInit {
 
-  imagePreview: string;
+
+  galleri: Galleri;
   form: FormGroup;
-  private mode:'create';
+  imagePreview: string;
+  private mode = 'create';
+  private galleriId: string;
 
-  constructor( public galleriServise: GalleriService ) {}
+  constructor(
+    public galleriServise: GalleriService,
+    public route : ActivatedRoute
+    ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.form = new FormGroup({
       title: new FormControl(null, {
-        validators:[Validators.required, Validators.minLength(4)]
+        validators: [Validators.required, Validators.minLength(4)],
       }),
-      description: new FormControl(null, {validators: [Validators.required]}),
-      image: new FormControl(null,{
-        validators:[Validators.required],
-        asyncValidators:[mimeType]
-
+      description: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType]
       })
+    });
+    this.route.paramMap.subscribe((paramMap: ParamMap) =>{
+      if(paramMap.has("galleriId")) {
+        this.mode = "edit";
+        this.galleriId = paramMap.get("galleriId");
+        this.galleriServise.getMedia(this.galleriId).subscribe(galleriData => {
+          this.galleri = {
+            id: galleriData._id,
+            title: galleriData.title,
+            imagePath: galleriData.imagePath,
+            description: galleriData.description
+          };
+          this.form.setValue({
+            title:this.galleri.title,
+            image: this.galleri.imagePath,
+            description: this.galleri.description
+          });
+        });
+      } else{
+        this.mode = "create";
+        this.galleriId = null;
+      }
     })
   }
 
-  onImagePicked(event: Event){
+  onImagePicked(event: Event) {
     const file = (event.target as HTMLInputElement).files[0];
-    this.form.patchValue({image: file});
-    this.form.get("image").updateValueAndValidity();
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
     const reader = new FileReader();
     reader.onload = () => {
       this.imagePreview = reader.result as string;
@@ -42,7 +72,7 @@ export class PersonalGalleriComponent implements OnInit {
     reader.readAsDataURL(file);
   }
 
-  onSavePost(){
+  onSavePost() {
     if (this.form.invalid) {
       return;
     }
@@ -52,8 +82,14 @@ export class PersonalGalleriComponent implements OnInit {
         this.form.value.image,
         this.form.value.description
       );
-
-    };
+    } else {
+      this.galleriServise.updateMedia(
+        this.galleriId,
+        this.form.value.title,
+        this.form.value.image,
+        this.form.value.description
+      );
+    }
     this.form.reset();
   }
 }
